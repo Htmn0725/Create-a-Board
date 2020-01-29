b8afa64ba398b905c1d8ad632c5083a185bcc1fa
hitomi.s <lak.bay.7@outlook.jp>
2020-01-30 00:00:19 +0900

    入力したデータを取得し、ファイルに書き込む処理まで実装

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
-<?php echo 'Hello World';
+<?php
+
+// メッセージを保存するファイルのパス設定
+define( 'FILENAME', './message.txt');
+
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+if( !empty($_POST['btn_submit']) ) {
+	
+	if( $file_handle = fopen( FILENAME, "a") ) {
+
+	    // 日時を取得
+		$now_date = date("Y-m-d H:i:s");
+	
+		// 書き込むデータを作成
+		$data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
+	
+		// 書き込み
+		fwrite( $file_handle, $data);
+	
+		// ファイルを閉じる
+		fclose( $file_handle);
+	}		
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ひとり言掲示板</title>
+<link href="./create-a-board.css" rel="stylesheet">
+</head>
+<body>
+<h1>ひとり言掲示板</h1>
+<form method="post">
+  <div>
+    <label for="view_name">name</label>
+    <input id="view_name" type="text" name="view_name" value="">
+  </div>
+  <div>
+    <label for="message">message</label>
+    <textarea id="message" name="message"></textarea>
+  </div>
+  <input type="submit" name="btn_submit" value="go!">
+</form>
+<hr>
+<section>
+<!-- ここに投稿されたメッセージを表示 -->
+</section>
+</body>
+</html>
