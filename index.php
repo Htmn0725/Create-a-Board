6c015e4623c1b3701716b3886694b136b5cce279
hitomi.s <lak.bay.7@outlook.jp>
2020-01-31 00:05:59 +0900

    投稿したメッセージを画面下部に表示。投稿時にバリデーションを実施、エラーメッセージと投稿完了メッセージを追加。

--- index.php
+++ index.php
@@ -6,23 +6,76 @@ define( 'FILENAME', './message.txt');
 // タイムゾーン設定
 date_default_timezone_set('Asia/Tokyo');
 
-if( !empty($_POST['btn_submit']) ) {
-	
-	if( $file_handle = fopen( FILENAME, "a") ) {
+// 初期化
+$error_message = array();
+$clean = array();
+$now_date = null;
+$data = null;
+$file_handle = null;
+$split_data = null;
+$message = array();
+$message_array = array();
+$success_message = null;
 
-	    // 日時を取得
-		$now_date = date("Y-m-d H:i:s");
+if( !empty($_POST['btn_submit']) ) {
+
+	// 入力チェック
+	// name
+	if( empty($_POST['view_name']) ){
+		$error_message[] = 'Please enter name.';
+	}
+	else{
+		$clean['view_name'] = htmlspecialchars( $_POST['view_name'], ENT_QUOTES);
+		$clean['view_name'] = preg_replace( '/\\r\\n|\\n|\\r/', '', 
+		$clean['view_name']);
+	}
 	
-		// 書き込むデータを作成
-		$data = "'".$_POST['view_name']."','".$_POST['message']."','".$now_date."'\n";
-	
-		// 書き込み
-		fwrite( $file_handle, $data);
-	
-		// ファイルを閉じる
-		fclose( $file_handle);
+	// message
+	if( empty($_POST['message']) ){
+		$error_message[] = 'Please enter message.';
+	}
+	else{
+		$clean['message'] = htmlspecialchars( $_POST['message'], ENT_QUOTES);
+		$clean['message'] = preg_replace( '/\\r\\n|\\n|\\r/', '<br>', 
+		$clean['message']);
+	}
+
+	if( empty($error_message) ){
+		// nameとmessageを登録
+		if( $file_handle = fopen( FILENAME, "a") ) {
+
+			// 現在の日時を取得
+			$now_date = date("Y-m-d H:i:s");
+		
+			// 書き込むデータを作成
+			$data = "'".$clean['view_name']."','".$clean['message']."','".$now_date."'\n";
+		
+			fwrite( $file_handle, $data);
+		
+			fclose( $file_handle);
+
+			$success_message = 'Success!';
+		}
 	}		
 }
+
+if( $file_handle = fopen( FILENAME, "r")){
+
+	while( $data = fgets( $file_handle) ){
+		$split_data = preg_split( '/\'/', $data);
+
+		$message = array(
+			'view_name' => $split_data[1],
+			'message' => $split_data[3],
+			'post_date' => $split_data[5]
+		);
+		
+		array_unshift( $message_array, $message);
+	}
+
+	fclose( $file_handle);
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -33,6 +86,16 @@ if( !empty($_POST['btn_submit']) ) {
 </head>
 <body>
 <h1>ひとり言掲示板</h1>
+<?php if( !empty($success_message) ): ?>
+	<p class="success_message"><?php echo $success_message; ?></p>
+<?php endif;?>
+<?php if( !empty($error_message) ): ?>
+	<ul class="error_message">
+	  <?php foreach( $error_message as $value ): ?>
+		<li>・<?php echo $value; ?></li>
+	  <?php endforeach; ?>
+	</ul>
+<?php endif;?>
 <form method="post">
   <div>
     <label for="view_name">name</label>
@@ -46,7 +109,18 @@ if( !empty($_POST['btn_submit']) ) {
 </form>
 <hr>
 <section>
-<!-- ここに投稿されたメッセージを表示 -->
+<?php if( !empty($message_array) ): ?>
+<?php foreach( $message_array as $value ):?>
+<article>
+	<div class="info">
+		<h2><?php echo $value['view_name']; ?></h2>
+		<time><?php echo date('Y年m月d日　H:i',
+		strtotime($value['post_date'])); ?></time>
+	</div>
+	<p><?php echo $value['message']; ?></p>
+</article>
+<?php endforeach; ?>
+<?php endif; ?>
 </section>
 </body>
 </html>
